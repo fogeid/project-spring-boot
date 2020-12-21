772c9570cf57a7b70a120fd4669995a6112e09f3
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2020-12-21 14:11:46 -0400

    User entity and resource

---
+++ src/main/java/io/github/fogeid/curso/resources/UserResources.java
@@ -0,0 +1,19 @@
+package io.github.fogeid.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.fogeid.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Diego", "diego.silva@aluno.ic.ufmt.br", "65992697045", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
