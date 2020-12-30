6d2dc7a19e7b423f31e020e08c2510ebfa5680e4
Diego Batista <diego.silva@aluno.ic.ufmt.br>
2020-12-30 11:40:53 -0400

    Many-to-many association with JoinTable

--- src/main/java/io/github/fogeid/curso/config/TesteConfig.java
+++ src/main/java/io/github/fogeid/curso/config/TesteConfig.java
@@ -49,6 +49,15 @@ public class TesteConfig implements CommandLineRunner {
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));		
+		
 		User u1 = new User(null, "Diego Batista", "diego.batista@gmail.com", "992697045", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
